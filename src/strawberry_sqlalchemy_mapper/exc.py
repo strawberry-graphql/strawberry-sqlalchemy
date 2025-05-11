dc7aa613ee9d75eb4ddce241391b46993610349a
Ckk3 <contato.gustavomota@gmail.com>
2025-05-11 14:09:38 +0000

    Run pre-commit in exc.py

--- src/strawberry_sqlalchemy_mapper/exc.py
+++ src/strawberry_sqlalchemy_mapper/exc.py
@@ -2,7 +2,7 @@ class UnsupportedColumnType(Exception):
     def __init__(self, key, type):
         super().__init__(
             f"Unsupported column type: `{type}` on column: `{key}`. "
-            + "Possible fix: exclude this column"
+            "Possible fix: exclude this column"
         )
 
 
@@ -10,8 +10,8 @@ class UnsupportedAssociationProxyTarget(Exception):
     def __init__(self, key):
         super().__init__(
             f"Association proxy `{key}` is expected to be of form "
-            + "association_proxy(relationship_name, other relationship name). "
-            + "Ensure it matches the expected form or add this association proxy to __exclude__."
+            "association_proxy(relationship_name, other relationship name). "
+            "Ensure it matches the expected form or add this association proxy to __exclude__."
         )
 
 
@@ -19,7 +19,7 @@ class HybridPropertyNotAnnotated(Exception):
     def __init__(self, key):
         super().__init__(
             f"Descriptor `{key}` is a hybrid property, but does not have an "
-            + "annotated return type"
+            "annotated return type"
         )
 
 
@@ -27,7 +27,7 @@ class UnsupportedDescriptorType(Exception):
     def __init__(self, key):
         super().__init__(
             f"Descriptor `{key}` is expected to be a column, relationship, "
-            + "or association proxy."
+            "or association proxy."
         )
 
 
@@ -35,5 +35,5 @@ class InterfaceModelNotPolymorphic(Exception):
     def __init__(self, model):
         super().__init__(
             f"Model `{model}` is not polymorphic or is not the base model of its "
-            + "inheritance chain, and thus cannot be used as an interface."
+            "inheritance chain, and thus cannot be used as an interface."
         )
