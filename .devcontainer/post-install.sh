7f2cbafd067b62700a720cf1a4c69b97c438301d
Luis Gustavo <contato.gustavomota@gmail.com>
2025-05-14 18:51:50 -0300

    Update devcontainer setup (#246)
    * Update devcontainer setup
    
    * using ruff instead of black extension

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,2 +1,10 @@
-poetry install
+#!/bin/bash
+set -e  # Exit on error
+
+echo "📦 Installing Python dependencies with Poetry..."
+poetry config virtualenvs.in-project true
+poetry install --no-cache
+
+echo "🔧 Setting up pre-commit hooks..."
+
 pre-commit install --install-hooks
